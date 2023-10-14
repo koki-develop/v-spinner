c84d90d34f338b40fe6e4cda80662584055e02d6
koki-develop <kou.pg.0131@gmail.com>
2023-10-14 11:54:28 +0900

    Create prefix_suffix example

---
+++ examples/src/prefix_suffix/main.v
@@ -0,0 +1,20 @@
+module main
+
+import time
+import spinner
+
+fn main() {
+	mut spin := spinner.new(spinner.character_sets[0],
+		prefix: '[PREFIX] ' // optional
+		suffix: ' starting up...' // optional
+	)
+
+	spin.start() // Display the spinner
+	time.sleep(2 * time.second)
+
+	// It's also possible to change in the middle
+	spin.suffix = ' shutting down...'
+
+	time.sleep(2 * time.second)
+	spin.stop()
+}
