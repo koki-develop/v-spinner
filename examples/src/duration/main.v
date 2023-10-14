1a7549e8b43d0e94903d77fb88646e0e3aa2252b
koki-develop <kou.pg.0131@gmail.com>
2023-10-14 11:58:07 +0900

    Create duration example

---
+++ examples/src/duration/main.v
@@ -0,0 +1,19 @@
+module main
+
+import time
+import spinner
+
+fn main() {
+	mut spin := spinner.new(spinner.character_sets[0],
+		duration: 500 * time.millisecond
+	)
+
+	spin.start() // Display the spinner
+	time.sleep(2 * time.second)
+
+	// It's also possible to change in the middle
+	spin.duration = 50 * time.millisecond
+
+	time.sleep(2 * time.second)
+	spin.stop()
+}
