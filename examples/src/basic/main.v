377ea3658916371b8664451652cf424b7696c628
koki-develop <kou.pg.0131@gmail.com>
2023-10-14 12:25:21 +0900

    character_sets[0] -> slash_1

--- examples/src/basic/main.v
+++ examples/src/basic/main.v
@@ -4,7 +4,7 @@ import time
 import spinner
 
 fn main() {
-	mut spin := spinner.new(spinner.character_sets[0])
+	mut spin := spinner.new(spinner.slash_1)
 
 	spin.start() // Display the spinner
 	time.sleep(3 * time.second)
